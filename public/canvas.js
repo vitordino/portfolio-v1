57fe100db560ad738e4da052c0839884acf23168
Vitor Dino <vitor@kunstdesign.com.br>
2017-11-30 11:01:34 -0200

    canvas rewrite

--- public/canvas.js
+++ public/canvas.js
@@ -1,42 +1,38 @@
-var locs = [];
+var locs = []
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	canvas.parent('sketch');
+	var canvas = createCanvas(windowWidth, (windowHeight*0.8))
+	canvas.parent('sketch')
 
-	var res = 24;
-	var countX = ceil(width/res) + 1;
-	var countY = ceil(height/res) + 1;
-
-	var res = 20;
-	var countX = ceil(width / res) + 1;
-	var countY = ceil(height / res) + 1;
+	var res = 20
+	var countX = ceil(width / res) + 1
+	var countY = ceil(height / res) + 1
 
 	for (var j = 0; j < countY; j++) {
 		for (var i = 0; i < countX; i++) {
-			locs.push(new p5.Vector(res * i, res * j));
+			locs.push(new p5.Vector(res * i, res * j))
 		}
 	}
 
-	noFill();
-	stroke(220, 102, 102);
+	noFill()
+	stroke(window.HEADER_LINE_COLOR || '#dc6666')
 }
 
 function draw() {
-	background(18, 18, 19);
+	background('#121213')
 	for (var i = locs.length - 1; i >= 0; i--) {
-		var h = calcVec(locs[i].x - mouseX, locs[i].y - mouseY);
+		var h = calcVec(locs[i].x - mouseX, locs[i].y - mouseY)
 		line(
 			locs[i].x,
 			locs[i].y,
 			locs[i].x + 15 * cos(h.heading()),
 			locs[i].y + 15 * sin(h.heading())
-		);
+		)
 	}
 }
 
 function calcVec(x, y) {
-	return new p5.Vector(y - x, -x - y);
+	return new p5.Vector(y - x, -x - y)
 }
 
-// [TODO]: resize canvas on window resize
+//- [TODO]: resize canvas on resize window
